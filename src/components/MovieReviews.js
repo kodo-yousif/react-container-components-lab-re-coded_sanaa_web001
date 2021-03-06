e7663894a4eda96575601bea898e6791736e9f49
Kodo Yousif <kodo.00257248@gmail.com>
2021-03-06 09:35:20 +0000

    Automatically backed up by Learn

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from 'react';
 
 const Review = ({
@@ -34,3 +35,41 @@ MovieReviews.defaultProps = {
 };
 
 export default MovieReviews;
+=======
+import React, { Component } from 'react';
+import 'isomorphic-fetch';
+
+import MovieReviews from './MovieReviews';
+
+const NYT_API_KEY = 'f98593a095b44546bf4073744b540da0';
+const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
+            + `api-key=${NYT_API_KEY}`;
+
+class LatestMovieReviewsContainer extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      reviews: []
+    };
+  }
+
+  componentDidMount() {
+    fetch(URL)
+      .then(res => res.json())
+      .then(response => this.setState({ reviews: response.results }));
+  }
+
+  render() {
+    return (
+      <div className="latest-movie-reviews">
+        <h2>The Latest Reviews:</h2>
+        <MovieReviews reviews={this.state.reviews} />
+      </div>
+    );
+  }
+}
+
+
+export default LatestMovieReviewsContainer;
+>>>>>>> 7c5bf6e2ac934fd7b3be732ccc1f5d0e781feaf4
